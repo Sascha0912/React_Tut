f0013c5b4d2a59d51b635c1a3aaa3f0961d0e0a7
Sascha Obst <mail@saschaobst.de>
2023-08-07 23:50:46 +0200

    Tutorial #8

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// import reportWebVitals from './reportWebVitals';
+
+import Clock from "./pages/Clock/clock";
+// import MagicButton from "./pages/MagicTable/MagicButton";
+import MagicTable from "./pages/MagicTable/MagicTable";
 import CurrencyCalculator from './pages/CurrencyCalculator/CurrencyCalculator';
 
 function App() {
   return (
-    <CurrencyCalculator/>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Clock />} />
+        <Route path="/button" element={<MagicTable />} />
+        <Route path='/currency' element={<CurrencyCalculator/>} />
+      </Routes>
+    </Router>
   );
 }
 
